fa9ad94d7a3f8e65e6bd8fe372dd5c4252c55051
David <69402666+Social713@users.noreply.github.com>
2021-04-20 23:57:42 -0400

    Add files via upload
    updated face cards

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-
+/**
+ * Credits
+ * https://www.w3schools.com/html/html_images.asp
+ * https://www.sitepoint.com/community/t/using-images-in-array-object/219080/2
+ * https://learnersbucket.com/examples/html/3-different-ways-to-hide-dom-element-using-javascript/
+ * https://www.youtube.com/watch?v=oT49KkhOv-Y&t=1506s
+ * http://web2.acbl.org/documentlibrary/marketing/Clip_Art/cards_png_zip.zip
+ * https://www.judgecasino.com/blackjack/number-of-decks-in-blackjack/
+ */
 // let playerName = prompt(`What is your name?`);
+
+// global variables
+
 let playerHand =[];
 let dealerHand = [];
 let hitButton = document.querySelector("#hit-me");
@@ -10,40 +21,86 @@ let dGoldMedal = document.querySelector(`.dealer-gold`);
 let pSliverMedal = document.querySelector(`.player-silver`);
 let dSliverMedal = document.querySelector(`.dealer-silver`);
 
+// casinos use multiple decks for blackjack to stop card counters, so no need to pop cards out of the deck.
 const deck = [
     2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
     2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
     2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
     2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 
+    // insert two blank spaces so the deck[i] will access the proper cardImage[i]
 const cardImages = [
-    "", "", `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `J`, `Q`, `K`, `A`];
+    "", "", `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `A`];
+
+    // suits need to be randomized then added to cardImages
 const suits = ['S', 'H', 'C', 'D'];
 
-    // '<img src="' + cardImages[playerHand[0]] + '">';
+    // randomization of deck, suits, and thus cardImages (inside startGame())
 const randomCard = (deck) => deck[Math.floor(Math.random() * deck.length)];
-const randomSuit = (suits) => suits[Math.floor(Math.random()*suits.length)];
-// randomSuit(suits);
+const randomSuit = (suits) => suits[Math.floor(Math.random() * suits.length)];
+
+/////////// Q,K,A not popping up!!!
+
+const faceCardArry = ["10", "J", "Q", "K"];
+const randomFace = (face) => faceCardArry[Math.floor(Math.random() * faceCardArry.length)];
+const pHandGen = () =>{
+// example <img src="images/cards/3S.png">
+// console.log(cardImages[playerHand[0]] + " and " + cardImages[playerHand[1]]);
+    if(cardImages[playerHand[0]]==10 && cardImages[playerHand[1]]==10){
+        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
+        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+    }
+    if (cardImages[playerHand[0]]==10){
+        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
+        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+    }
+    if (cardImages[playerHand[1]]==10){
+        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
+        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+    }
+    else {
+        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
+        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+    }
+}
+
+const dHandGen = () =>{
+    if(cardImages[dealerHand[0]]==10 && cardImages[dealerHand[1]]==10){
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
+    }
+    if (cardImages[dealerHand[0]]==10){
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[dealerHand[1]] + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
+    }
+    if (cardImages[dealerHand[1]]==10){
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[dealerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
+    }
+    else {
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[dealerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[dealerHand[1]] + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
+    }
+}
+// app load functionality as well as restart button functionality
 const startGame = () => {
     pGoldMedal.style.display = 'none';
     dGoldMedal.style.display = 'none';
     pSliverMedal.style.display = 'none';
     dSliverMedal.style.display = 'none';
     playerHand = [randomCard(deck), randomCard(deck)];
-    dealerHand = [randomCard(deck), randomCard(deck)];     // <img src="images/cards/3S.png">
-    document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
-    console.log(cardImages[playerHand[0]] + " and " + cardImages[playerHand[1]]);
-    document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;;
-    document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
-    document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[dealerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[dealerHand[1]] + randomSuit(suits) +  '.png">';;
-    document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
+    dealerHand = [randomCard(deck), randomCard(deck)];    
+    pHandGen();
+    dHandGen();
 };
 
 
-// ACE LOGIC (I think...) 
-// if(playerHand.includes(11)&& pHandValue()>21) pHandValue() = pHandValue()-10;
+//////////////////////////// ACE LOGIC (I think...) ///////////////////////////////
+// if(playerHand.includes(11)&& pHandValue()>21) pHandValue() = pHandValue()-10; //
+///////////////////////////////////////////////////////////////////////////////////
 
 
+// calculations for player and dealer hand values
 const pHandValue = () =>{
     return playerHand.reduce((acc, ccV)=>{
         if(acc+ccV>21){
@@ -62,6 +119,8 @@ const dHandValue = () =>{
         return acc += ccV;
     })
 }
+
+// functionality for hit me button
 const hitMe = () => {
     // i = playerHand.length
     playerHand.push(randomCard(deck));
@@ -72,6 +131,11 @@ const hitMe = () => {
     if(playerHand.includes(11)&& pHandValue()>21) pHandValue() = pHandValue()-10;
 };
 
+
+
+// Game starts below here
+
+
 startGame();
 
 hitButton.addEventListener("click", hitMe);
@@ -80,6 +144,7 @@ stayButton.addEventListener(`click`, () =>{
     
     while(dHandValue()<19){
         dealerHand.push(randomCard(deck));
+        // add the card images in addition to the values being pushed
         for(let i=2; i<dealerHand.length;i+=1){
             document.querySelector(".dealer-hand").innerHTML += '<img class="cards" src="images/cards/' + cardImages[dealerHand[i]] + randomSuit(suits) + '.png">';
             document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
@@ -136,7 +201,6 @@ stayButton.addEventListener(`click`, () =>{
 });
 
 restartButton.addEventListener('click',()=>{
-    console.log('restart!!!')
     startGame();
     hitButton.style.display = 'inline-block';
     stayButton.style.display = 'inline';
