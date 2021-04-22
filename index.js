78a34482887dcac381604c9a714903ded04a50a4
David <69402666+Social713@users.noreply.github.com>
2021-04-22 01:24:32 -0400

    Add files via upload
    Ace Logic is complete

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ let pSliverMedal = document.querySelector(`.player-silver`);
 let dSliverMedal = document.querySelector(`.dealer-silver`);
 
 // casinos use multiple decks for blackjack to stop card counters, so no need to pop cards out of the deck.
+
 const deck = [
     2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
     2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
@@ -35,46 +36,52 @@ const cardImages = [
     // suits need to be randomized then added to cardImages
 const suits = ['S', 'H', 'C', 'D'];
 
-    // randomization of deck, suits, and thus cardImages (inside startGame())
+// randomization of deck, suits, and thus cardImages (inside startGame())
 const randomCard = (deck) => deck[Math.floor(Math.random() * deck.length)];
 const randomSuit = (suits) => suits[Math.floor(Math.random() * suits.length)];
 
-/////////// Q,K,A not popping up!!!
+/////////// Q,K,A not popping up!!! <-- solved!
 
-const faceCardArry = ["10", "J", "Q", "K"];
-const randomFace = (face) => faceCardArry[Math.floor(Math.random() * faceCardArry.length)];
+const faceCard = ["10", "J", "Q", "K"];
+const randomFace = (face) => faceCard[Math.floor(Math.random() * faceCard.length)];
 const pHandGen = () =>{
 // example <img src="images/cards/3S.png">
 // console.log(cardImages[playerHand[0]] + " and " + cardImages[playerHand[1]]);
-    if(cardImages[playerHand[0]]==10 && cardImages[playerHand[1]]==10){
-        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
-        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
-    }
-    if (cardImages[playerHand[0]]==10){
-        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
-        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
-    }
-    if (cardImages[playerHand[1]]==10){
-        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
-        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
-    }
-    else {
-        document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
-        document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
-    }
+
+        if (cardImages[playerHand[0]]==10 && cardImages[playerHand[1]]==10){
+            document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) +  '.png">';
+            document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+        }
+        if (cardImages[playerHand[0]]==10){
+            document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
+            document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+        }
+        if (cardImages[playerHand[1]]==10){
+            document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) +  '.png">';
+            document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+        }
+        else {
+            document.querySelector(".player-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[playerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[playerHand[1]] + randomSuit(suits) +  '.png">';
+            document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
+        }
+    
 }
 
+
 const dHandGen = () =>{
+
+ ///// ace logic here too!!! ////
+    
     if(cardImages[dealerHand[0]]==10 && cardImages[dealerHand[1]]==10){
-        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) +  '.png">';
         document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
     }
     if (cardImages[dealerHand[0]]==10){
-        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[dealerHand[1]] + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + cardImages[dealerHand[1]] + randomSuit(suits) +  '.png">';
         document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
     }
     if (cardImages[dealerHand[1]]==10){
-        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[dealerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCardArry) + randomSuit(suits) +  '.png">';
+        document.querySelector(".dealer-hand").innerHTML = '<img class="cards" src="images/cards/' + cardImages[dealerHand[0]] + randomSuit(suits) + '.png">' + '<img class="cards" src="images/cards/' + randomFace(faceCard) + randomSuit(suits) +  '.png">';
         document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
     }
     else {
@@ -92,6 +99,8 @@ const startGame = () => {
     dealerHand = [randomCard(deck), randomCard(deck)];    
     pHandGen();
     dHandGen();
+    hitCount = 2;
+    stayCount = 2;
 };
 
 
@@ -100,9 +109,14 @@ const startGame = () => {
 ///////////////////////////////////////////////////////////////////////////////////
 
 
+
+
 // calculations for player and dealer hand values
 const pHandValue = () =>{
     return playerHand.reduce((acc, ccV)=>{
+        if (ccV===11 && acc>10){
+            return acc = acc+ccV - 10;
+        }
         if(acc+ccV>21){
             // alert(`Sorry, ${playerName}, but the dealer won.`);
             hitButton.style.display = 'none';
@@ -116,21 +130,28 @@ const pHandValue = () =>{
 }
 const dHandValue = () =>{
     return dealerHand.reduce((acc, ccV)=>{
+        if (ccV===11 && acc>10){
+            return acc = acc+ccV - 10;
+        }
         return acc += ccV;
     })
 }
 
 // functionality for hit me button
+let hitCount = 2;
+let stayCount = 2;
 const hitMe = () => {
-    // i = playerHand.length
+    // i = 2, because 2 cards
     playerHand.push(randomCard(deck));
-    for(let i = 2; i<playerHand.length; i+=1){
-        document.querySelector(".player-hand").innerHTML += '<img class="cards" src="images/cards/' + cardImages[playerHand[i]] + randomSuit(suits) + '.png">';
+   
+        document.querySelector(".player-hand").innerHTML += '<img class="cards" src="images/cards/' + cardImages[playerHand[hitCount]] + randomSuit(suits) + '.png">';
         document.querySelector(".player-value").innerHTML = `Total: ${pHandValue()}`;
-    }
-    if(playerHand.includes(11)&& pHandValue()>21) pHandValue() = pHandValue()-10;
+        console.log("cardImages: "+ cardImages[playerHand[hitCount]])
+        console.log(`i: `+ hitCount)
+        hitCount++;
+    
+    // if(playerHand.includes(11)&& pHandValue()>21) pHandValue() = pHandValue()-10;
 };
-
 
 
 // Game starts below here
@@ -145,19 +166,35 @@ stayButton.addEventListener(`click`, () =>{
     while(dHandValue()<pHandValue() && dHandValue()<19){
         dealerHand.push(randomCard(deck));
         // add the card images in addition to the values being pushed
-        for(let i=2; i<dealerHand.length;i+=1){
-            document.querySelector(".dealer-hand").innerHTML += '<img class="cards" src="images/cards/' + cardImages[dealerHand[i]] + randomSuit(suits) + '.png">';
+           
+            document.querySelector(".dealer-hand").innerHTML += '<img class="cards" src="images/cards/' + cardImages[dealerHand[stayCount]] + randomSuit(suits) + '.png">';
             document.querySelector(".dealer-value").innerHTML = `Total: ${dHandValue()}`;
+            console.log("cardImages: "+ cardImages[dealerHand[stayCount]])
+            console.log(`i: `+ stayCount)
+            stayCount++;
         }
         
-    }
 
     if(pHandValue()===dHandValue()){
-        alert(`Draw!`);
-        hitButton.style.display = 'none';
-        stayButton.style.display = 'none';
-        pGoldMedal.style.display = 'inline';
-        dGoldMedal.style.display = 'inline';
+        // alert(`Draw!`);
+        if(playerHand.length>dealerHand.length){
+            hitButton.style.display = 'none';
+            stayButton.style.display = 'none';
+            pSliverMedal.style.display = 'inline';
+            dGoldMedal.style.display = 'inline';    
+        }
+        if(playerHand.length<dealerHand.length){
+            hitButton.style.display = 'none';
+            stayButton.style.display = 'none';
+            dSliverMedal.style.display = 'inline';
+            pGoldMedal.style.display = 'inline';    
+        }
+        else{
+            hitButton.style.display = 'none';
+            stayButton.style.display = 'none';
+            pGoldMedal.style.display = 'inline';
+            dGoldMedal.style.display = 'inline';
+        }
     } 
 
     if(dHandValue()>=19){
@@ -168,11 +205,6 @@ stayButton.addEventListener(`click`, () =>{
             pGoldMedal.style.display = 'inline';
             dSliverMedal.style.display = 'inline';
         }
-        // if(dHandValue()===21) {
-        //     alert(`Sorry, ${playerName}, but the dealer won. Blackjack!`);
-        //     hitButton.style.display = 'none';
-        //     stayButton.style.display = 'none';
-        // }
     }
 
     if(dHandValue()<=21 && dHandValue()>pHandValue()) {
